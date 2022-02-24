4c124f63a5cf46e6d474754ef526860667d8af02
Naresh-Annam <92820809+Naresh-Annam@users.noreply.github.com>
2022-02-24 11:48:45 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,7 @@
+package com.caseStudy1.calculator;
+
+public class Division {
+	public Division(double num1, double num2) {
+		System.out.println("The Division value of "+num1+" and "+num2+" is "+(num1 / num2));
+	}
+}
---
+++ Subraction.java
@@ -0,0 +1,7 @@
+package com.caseStudy1.calculator;
+
+public class Subraction {
+	Subraction(int num1, int num2) {
+		System.out.println("The Subraction value of " + num1 + " and " + num2 + " is " + (num1 - num2));
+	}
+}
