54452c26c29a2487c8773bf3dd3a51b2974871d8
Naresh-Annam <92820809+Naresh-Annam@users.noreply.github.com>
2022-02-24 12:36:31 +0530

    Delete Division.java

--- Division.java
+++
@@ -1,7 +0,0 @@
-package com.caseStudy1.calculator;
-
-public class Division {
-	public Division(double num1, double num2) {
-		System.out.println("The Division value of "+num1+" and "+num2+" is "+(num1 / num2));
-	}
-}
