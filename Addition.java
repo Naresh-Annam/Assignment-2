3deb1cac3b8210fcaee5fffa5185deb8bb694bf5
Naresh-Annam <92820809+Naresh-Annam@users.noreply.github.com>
2022-02-24 12:36:09 +0530

    Delete Addition.java

--- Addition.java
+++
@@ -1,7 +0,0 @@
-package com.caseStudy1.calculator;
-
-public class Addition {
-	Addition(int num1,int num2) {
-		System.out.println("The Addiition value of "+num1+" and "+num2+" is "+(num1+num2));
-	}
-}
